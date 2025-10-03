3045fcbf1294f80286047e5adcea9599c7d9035c
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-10-03 17:56:19 +0500

    Create models.py

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -6,6 +7,11 @@ class User(Base):
     __tablename__ = "users"
 
     # column larni yarating
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     posts = relationship("Post", back_populates="user")
     comments = relationship("Comment", back_populates="user")
@@ -16,6 +22,12 @@ class Post(Base):
 
     # column larni yarating
 
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(200))
+    body = Column(Text)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
     user = relationship("User", back_populates="posts")
     comments = relationship("Comment", back_populates="post")
 
@@ -24,15 +36,14 @@ class Comment(Base):
     __tablename__ = "comments"
 
     # column larni yarating
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(Text)
+    post_id = Column(Integer, ForeignKey("posts.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
   
     user = relationship("User", back_populates="comments")
     post = relationship("Post", back_populates="comments")
-
-
-
-
-
-
-
 
 
