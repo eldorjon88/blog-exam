b0db0ff3287531920ddfbe9f3e3afb947bf657b1
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-10-03 18:32:49 +0500

    Created main.py

--- main.py
+++ main.py
@@ -37,3 +37,19 @@ if __name__ == "__main__":
     init_db()
     load_demo_data()
     print("✅ Database initialized and demo data loaded!")
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+import os
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "eldorjon0704")
+DB_NAME = os.getenv("DB_NAME", "exam")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+Base = declarative_base()
