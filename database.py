e8fe8639216e6f50e0a15351fb61eb854e3ad35e
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-10-03 18:07:40 +0500

    Create database.py

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 # engine, LocalSession, Base larni yarating
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import Config
 
-
-
-
-
-
-
+engine = create_engine(Config.DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
