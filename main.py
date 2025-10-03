ea774ef348f3e13eeeb7159a2e3fd38c80cee1c8
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-10-03 17:05:58 +0500

    Create main.py

--- main.py
+++ main.py
@@ -38,22 +38,6 @@ if __name__ == "__main__":
 
 
 
-import json
-from database import Base, engine, SessionLocal
-from models import User, Post, Comment
-import crud
-
-
-def init_db():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-
-
-def load_demo_data():
-    db = SessionLocal()
-    with open("demo_data.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
-
     for u in data.get("users", []):
         crud.create_user(db, u["username"], u["email"])
 
@@ -63,10 +47,10 @@ def load_demo_data():
     for c in data.get("comments", []):
         crud.create_comment(db, c["user_id"], c["post_id"], c["text"])
 
-    db.close()
+    # db.close()
 
 
-if __name__ == "__main__":
-    init_db()
-    load_demo_data()
-    print("✅ Database initialized and demo data loaded!")
+# if __name__ == "__main__":
+#     init_db()
+#     load_demo_data()
+#     print("✅ Database initialized and demo data loaded!")
